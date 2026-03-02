17710a761a3cd319f1519ae51dafbd3960003665
Emily Morgan <eakimbrell@gmail.com>
2026-03-02 11:26:38 -0700

    connect other pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-//   base: "/arowynn.github.io"
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        photography: resolve(__dirname, 'photography.html'),
+        'sat-tycoon': resolve(__dirname, 'sat-tycoon.html'),
+        'auburn-hacks': resolve(__dirname, 'auburn-hacks.html'),
+      },
+    },
+  },
 });
